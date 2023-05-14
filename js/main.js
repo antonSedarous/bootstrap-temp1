e129126c22f0e40fc5574da6234b3dfa77cbceed
antonSedarous <tony.sedarous.azer@gmail.com>
2023-05-14 19:44:33 +0200

    create project

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(function (){
+    'use strict';
+
+//Adjust Slider Height
+var winH   = $(window).height(),
+    upperH =$('.upper-bar').innerHeight(),
+    navH   =$('.navbar').innerHeight(); 
+$('.slider,.carousel-item').height(winH - (upperH + navH) );
+
+
+
+
+
+
+
+
+});
